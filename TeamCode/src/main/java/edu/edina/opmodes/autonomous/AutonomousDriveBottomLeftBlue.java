8182d74dabfef6fd7e0b5c6f8b9b3a2b66121eff
Ratik-Jain <144185481+Ratik-Jain@users.noreply.github.com>
2023-11-17 21:24:41 -0600

    AutonomousBottomLeftBlue

---
+++ TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutonomousDriveBottomLeftBlue.java
@@ -0,0 +1,53 @@
+package edu.edina.opmodes.autonomous;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import edu.edina.library.subsystems.MecanumDrive;
+import edu.edina.library.util.Robot;
+
+/*
+ * This is a simple routine to test translational drive capabilities.
+ */
+@Config
+@Autonomous(name="BottomLeftBlue", group = "autonomous")
+public class AutonomousDriveBottomLeftBlue extends LinearOpMode {
+    protected Robot robot;
+    public double DISTANCE1 = 24;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new Robot(telemetry, hardwareMap, false);
+        robot.init();
+
+        //   robot.start();
+
+        waitForStart();
+
+        int i=0;
+        while(i<=372)
+        {
+            robot.MecanumDrive.driveToPose(-100,0,0);
+            i = i+1;
+        }
+
+
+////        drive.turn(Math.toRadians(90));
+////        drive.followTrajectory(autonomousTrajectory);
+
+//            while (opModeIsActive() && !isStopRequested()) {
+
+//
+//            }
+
+    }
+}
+//}
