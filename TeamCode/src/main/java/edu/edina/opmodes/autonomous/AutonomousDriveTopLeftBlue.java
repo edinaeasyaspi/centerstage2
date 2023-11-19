693eec3e0e87adc04c9a1e54efb02f8a54c4adba
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-11-19 16:34:56 -0600

    added time auto

--- TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutonomousDriveTopLeftBlue.java
+++ TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutonomousDriveTopLeftBlue.java
@@ -11,40 +11,54 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import edu.edina.library.subsystems.MecanumDrive;
-import edu.edina.library.util.Robot;
 
 /*
  * This is a simple routine to test translational drive capabilities.
  */
 @Config
-@Autonomous(name="TopLeftBlue", group = "autonomous")
+@Autonomous(group = "drive")
 public class AutonomousDriveTopLeftBlue extends LinearOpMode {
-    protected Robot robot;
-    public double DISTANCE1 = 24;
+    public double DISTANCE1 = 96;
 
     @Override
     public void runOpMode() throws InterruptedException {
+       // Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        robot = new Robot(telemetry, hardwareMap, false);
-        robot.init();
+        MecanumDrive drive = new MecanumDrive(hardwareMap);
 
-     //   robot.start();
+
+        Trajectory autonomousTrajectory = drive.trajectoryBuilder(new Pose2d())//-35,61
+                .lineToLinearHeading(new Pose2d(25, 0, Math.toRadians(0)))
+
+                // .splineTo(new Vector2d(-24,6), Math.toRadians(0))
+                // .splineTo(new Vector2d(-6,26), Math.toRadians(0))
+
+
+                //.strafeLeft(25)
+                // .forward(25)
+                // .strafeLeft(25)
+                // .back(25)
+                //.strafeLeft(45)
+                .build();
+
+
+//          //  TrajectoryBuilder trajectory = drive.trajectoryBuilder(new Pose2d());
+//          //  trajectory.strafeLeft(DISTANCE1);
+//          //  trajectory.build();
+
+      //  Pose2d poseEstimate = drive.getPoseEstimate();
+      //  telemetry.addData("finalX", poseEstimate.getX());
+      //  telemetry.addData("finalY", poseEstimate.getY());
+      //  telemetry.addData("finalHeading", poseEstimate.getHeading());
+      //  telemetry.update();
 
         waitForStart();
-
-        int i=0;
-        while(i<=172)
-        {
-            robot.MecanumDrive.driveToPose(-100,0,0);
-            i = i+1;
-        }
-
 
 ////        drive.turn(Math.toRadians(90));
 ////        drive.followTrajectory(autonomousTrajectory);
 
 //            while (opModeIsActive() && !isStopRequested()) {
-
+//                drive.followTrajectory(autonomousTrajectory);
 //
 //            }
 
