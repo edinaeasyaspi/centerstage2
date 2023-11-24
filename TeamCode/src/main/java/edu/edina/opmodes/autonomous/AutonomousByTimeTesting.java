62f0cc0c4f594e5aa29215081ed9a1d750fc27e4
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-11-23 22:55:30 -0600

    Changed some things

--- TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutonomousByTimeTesting.java
+++ TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutonomousByTimeTesting.java
@@ -1,45 +1,35 @@
 package edu.edina.opmodes.autonomous;
 
-
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-
-@Autonomous(name="Robot: Autonomous Drive by time", group="Autonomous")
-
+@Autonomous(name = "Robot: Autonomous Drive by time", group = "Autonomous")
 public class AutonomousByTimeTesting extends LinearOpMode {
 
-
-    private DcMotor       frontLeftMotor  = null;
-    private DcMotor       backLeftMotor  = null;
-
-    private DcMotor       frontRightMotor = null;
-
-    private DcMotor        backRightMotor = null;
-    private DcMotor     liftMotor = null;
-    private Servo   leftLiftServo = null;
-    private Servo   rightLiftServo = null;
+    private DcMotor frontLeftMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backRightMotor = null;
+    private DcMotor liftMotor = null;
+    private Servo leftLiftServo = null;
+    private Servo rightLiftServo = null;
     private CRServo leftIntakeServo = null;
     private CRServo rightIntakeServo = null;
 
-    private ElapsedTime     runtime = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
 
-
-    static final double     FORWARD_SPEED = 1.0;
-    static final double     TURN_SPEED    = 1.0;
+    static final double FORWARD_SPEED = 1.0;
+    static final double TURN_SPEED = 0.3;
 
     @Override
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        frontLeftMotor  = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
@@ -49,128 +39,64 @@ public class AutonomousByTimeTesting extends LinearOpMode {
         leftIntakeServo = hardwareMap.get(CRServo.class, "F2");
         rightIntakeServo = hardwareMap.get(CRServo.class, "F1");
 
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
         backRightMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
+        telemetry.addData("Status", "Ready to run");
         telemetry.update();
 
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
-
-        // Step 1:  Strafe right for 3 seconds
+        // Step 1: Strafe right for 3 seconds
         frontLeftMotor.setPower(FORWARD_SPEED);
         frontRightMotor.setPower(-FORWARD_SPEED);
         backLeftMotor.setPower(-FORWARD_SPEED);
         backRightMotor.setPower(FORWARD_SPEED);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
-            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
+        sleep(3000); // Sleep for 3 seconds
 
-
-        // Step 3:  Drive forward for 2 Seconds
+        // Step 2: Drive forward for 2 seconds
         frontLeftMotor.setPower(FORWARD_SPEED);
         frontRightMotor.setPower(FORWARD_SPEED);
         backLeftMotor.setPower(FORWARD_SPEED);
         backRightMotor.setPower(FORWARD_SPEED);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 2.0)) {
-            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
+        sleep(2000); // Sleep for 2 seconds
 
-
-        }
-
+        // Step 3: Intake servo for 1 second
         leftIntakeServo.setPower(1);
         rightIntakeServo.setPower(1);
-        while (opModeIsActive() && (runtime.seconds() < 1)) {
-            telemetry.addData("IntakeServo", "Leg 3: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
+        sleep(1000); // Sleep for 1 second
 
-        // Step 1: Turn 180 degrees
-        double turnStartTime = runtime.seconds();
-        double turnDuration = 1.5; // Adjust this value based on testing
-
+        // Step 4: Turn 180 degrees for 1.5 seconds
         frontLeftMotor.setPower(TURN_SPEED);
         frontRightMotor.setPower(-TURN_SPEED);
         backLeftMotor.setPower(TURN_SPEED);
         backRightMotor.setPower(-TURN_SPEED);
+        sleep(1500); // Sleep for 1.5 seconds
 
-        while (opModeIsActive() && (runtime.seconds() - turnStartTime < turnDuration)) {
-            telemetry.addData("Path", "Turning: %4.1f S Elapsed", runtime.seconds() - turnStartTime);
-            telemetry.update();
-            // Add a slight delay to give the motors time to act
-            sleep(50);
-        }
-
-// Stop the motors after turning
-        frontLeftMotor.setPower(0);
-        frontRightMotor.setPower(0);
-        backLeftMotor.setPower(0);
-        backRightMotor.setPower(0);
-
-
-
-
-    
-
+        // Step 5: Lift motor for 3 seconds
         liftMotor.setPower(FORWARD_SPEED);
-        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
-            telemetry.addData("Lift", "Leg 5: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
+        sleep(3000); // Sleep for 3 seconds
 
-        }
-
-
+        // Step 6: Lift servos for 1.5 seconds
         leftLiftServo.setPosition(1.0);
         rightLiftServo.setPosition(1.0);
-        while (opModeIsActive() && (runtime.seconds() < 1.5)) {
-            telemetry.addData("Servo Swing", "Leg 6: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
+        sleep(1500); // Sleep for 1.5 seconds
 
-        }
-
-
+        // Step 7: Intake servo reverse for 8 seconds
         leftIntakeServo.setPower(-1);
         rightIntakeServo.setPower(-1);
-        while (opModeIsActive() && (runtime.seconds() < 8)) {
-            telemetry.addData("IntakeServo", "Leg 7: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
+        sleep(8000); // Sleep for 8 seconds
 
+        // Step 8: Strafe left for 1 second
         frontLeftMotor.setPower(-FORWARD_SPEED);
         frontRightMotor.setPower(FORWARD_SPEED);
         backLeftMotor.setPower(FORWARD_SPEED);
         backRightMotor.setPower(-FORWARD_SPEED);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
-            telemetry.addData("Path", "Leg 8: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
+        sleep(1000); // Sleep for 1 second
 
-        frontLeftMotor.setPower(-FORWARD_SPEED);
-        frontRightMotor.setPower(-FORWARD_SPEED);
-        backLeftMotor.setPower(-FORWARD_SPEED);
-        backRightMotor.setPower(-FORWARD_SPEED);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
-            telemetry.addData("Path", "Leg 9: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-
-        // Step 4:  Stop
+        // Step 9: Stop all motors
         frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
@@ -180,7 +106,6 @@ public class AutonomousByTimeTesting extends LinearOpMode {
         rightIntakeServo.setPower(0);
         leftLiftServo.setPosition(0);
         rightLiftServo.setPosition(0);
-
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
