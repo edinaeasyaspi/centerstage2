314cc5ec7620a5dfbc961314e6e840e21ba9879a
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-11-30 18:24:59 -0600

    Revert "Added a color Sensor testing code"
    This reverts commit 30d3a53774b845cad1f60a6a56ff33071e83ef92.

--- TeamCode/src/main/java/edu/edina/library/util/RobotHardware.java
+++ TeamCode/src/main/java/edu/edina/library/util/RobotHardware.java
@@ -5,7 +5,6 @@ import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -24,8 +23,6 @@ public class RobotHardware {
     public final VoltageSensor voltageSensor;
 
     public final IMU imu;
-
-    public final ColorSensor colorSensor;
 
     public final Servo leftLiftServo, rightLiftServo;
 
@@ -60,6 +57,5 @@ public class RobotHardware {
         rightIntakeServo = hardwareMap.get(CRServo.class, "F1");
 
         liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
-        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
     }
 }
