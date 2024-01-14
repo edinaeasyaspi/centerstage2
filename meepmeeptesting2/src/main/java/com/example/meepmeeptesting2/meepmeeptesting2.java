a21d6ee4e223e69a028ad0db807206951fc53bc1
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2024-01-14 17:56:32 -0600

    update on meepmeep

--- meepmeeptesting2/src/main/java/com/example/meepmeeptesting2/meepmeeptesting2.java
+++ meepmeeptesting2/src/main/java/com/example/meepmeeptesting2/meepmeeptesting2.java
@@ -13,19 +13,41 @@ public class meepmeeptesting2 {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 16)
+                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 10)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-36, -70, 0))
                                 .turn(Math.toRadians(90))
                                 .forward(38)
                                 .back((10))
                                 .splineTo(new Vector2d(-58, -51), Math.toRadians(90))
-                                .splineTo(new Vector2d(-40, -10), Math.toRadians(0))
-                                .splineTo(new Vector2d(33, -13), Math.toRadians(0))
-                                .splineTo(new Vector2d(49, -34), Math.toRadians(0))
+                                .splineTo(new Vector2d(-57, -35), Math.toRadians(90))
+                                .turn(Math.toRadians(-90))
                                 .waitSeconds(1)
-                                .splineTo(new Vector2d(-45, -34), Math.toRadians(0))
-                                .waitSeconds(1)
+                                .turn(Math.toRadians(-90))
+                                .splineTo(new Vector2d(-54, -13), Math.toRadians(90))
+                                .splineTo(new Vector2d(-40, -11), Math.toRadians(0))
+                                .splineTo(new Vector2d(33, -12), Math.toRadians(0))
+                                .splineTo(new Vector2d(49, -35), Math.toRadians(0))
+                                .lineToLinearHeading(new Pose2d(-57, -35, Math.toRadians(180)))
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(49, -35, Math.toRadians(0)))
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(-57, -35, Math.toRadians(180)))
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(49, -35, Math.toRadians(0)))
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(-57, -35, Math.toRadians(180)))
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(49, -35, Math.toRadians(0)))
+
+
+
+
+                                //   .splineTo(new Vector2d(33, -13), Math.toRadians(0))
+                              //  .splineTo(new Vector2d(49, -34), Math.toRadians(0))
+                             //   .waitSeconds(1)
+                             //   .splineTo(new Vector2d(-45, -34), Math.toRadians(0))
+                             //   .waitSeconds(1)
 //.splineToConstantHeading(new Vector2d(40, 40), Math.toRadians(0))k
                                 //This is bottomrightredstraight
 
