feed158392bbb0880a062d6afb0bb76adcd89202
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2024-01-20 21:09:02 -0600

    white balancing in VisionTestMode

--- TeamCode/src/main/java/edu/edina/opmodes/teleop/test/VisionTestMode.java
+++ TeamCode/src/main/java/edu/edina/opmodes/teleop/test/VisionTestMode.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.WhiteBalanceControl;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
 
@@ -17,14 +18,22 @@ public class VisionTestMode extends LinearOpMode {
     @Override
     public void runOpMode() {
         ImageProcessor imageProcessor = new ImageProcessor(telemetry);
+
         VisionPortal.Builder visionPortalBuilder = new VisionPortal.Builder();
-        VisionPortal visionPortal = visionPortalBuilder.enableLiveView(true).
-                addProcessor((VisionProcessor) imageProcessor).
-                setCamera(hardwareMap.get(WebcamName.class, "LogitechC270_8034PI")).
-                setCameraResolution(new Size(640, 480)).
-                build();
+        VisionPortal visionPortal = visionPortalBuilder
+                .enableLiveView(true)
+                .addProcessor(imageProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "LogitechC270_8034PI"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
 
         while (opModeInInit()) {
+            if (visionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+                visionPortal
+                        .getCameraControl(WhiteBalanceControl.class)
+                        .setMode(WhiteBalanceControl.Mode.AUTO);
+            }
+
             ImageProcessor.Selected selectedSpike = imageProcessor.getSelection();
             telemetry.addData("Selected value", selectedSpike);
             telemetry.addData("diag", imageProcessor.getDiagString());
