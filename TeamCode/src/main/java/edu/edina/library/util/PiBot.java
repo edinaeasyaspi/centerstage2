e7bb559048e7a1233ee8ad32281622457265c03f
Ratik-Jain <144185481+Ratik-Jain@users.noreply.github.com>
2024-01-25 19:14:01 -0600

    PiBot class

---
+++ TeamCode/src/main/java/edu/edina/library/util/PiBot.java
@@ -0,0 +1,49 @@
+package edu.edina.library.util;
+
+import edu.edina.library.util.drivecontrol.DriveDirection;
+
+public class PiBot {
+    public void planDriveToClosestPoint(Point target, DriveDirection direction) {
+    }
+    public boolean runDrive() {
+        throw new RuntimeException();
+    }
+    public void rotateToHeadingThenStop() {
+
+    }
+
+    public void positionGrabber(/*GrabberPosition*/) {
+        //happy halloween
+    }
+    public boolean grabberInPosition() {
+        throw new RuntimeException();
+    }
+
+    public void grab(/*GrabberSide*/) {}
+    public void drop(/*GrabberSide*/) {}
+
+    public void positionHooks(){
+        //merry summer
+    }
+    public boolean hooksInPosition() {
+        throw new RuntimeException();
+    }
+
+    public void planLatchOnBackboard() {
+        //happy hanukkah
+    }
+    public boolean latchOn (){
+        throw new RuntimeException();
+    }
+    public void dropPixelOnBackground() {
+        //enum LEFTMIDDLERIGHT
+    }
+
+    public void intakePixel() {
+        //happy juneteenth
+    }
+
+    public void launch() {
+        //.
+    }
+}
