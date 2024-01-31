84d178fb97178c2573f8afc4ebbdb2f1f1c44d3f
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2024-01-30 20:01:53 -0600

    yeey

--- TeamCode/src/main/java/edu/edina/opmodes/teleop/test/LiftTest.java
+++ TeamCode/src/main/java/edu/edina/opmodes/teleop/test/LiftTest.java
@@ -1,29 +1,41 @@
 package edu.edina.opmodes.teleop.test;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp
-public class LiftTest extends OpMode {
+public class LiftTest extends LinearOpMode {
     public DcMotorEx liftMotor = null;
 
     @Override
-    public void init() {
-        DcMotorEx liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
+    public void runOpMode() throws InterruptedException {
+        liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad2.right_bumper) {
+                liftMotor.setTargetPosition(500);
+                liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                liftMotor.setPower(0.1);
+            } else liftMotor.setPower(0);
+        }
     }
-
-    @Override
-    public void loop() {
-        LiftTest();
-    }
-
-    public void LiftTest() {
-        if (gamepad2.back && gamepad2.start) {
-            liftMotor.setPower(-0.1);
-        }  else liftMotor.setPower(0);
-    }
-
 }
+
+
+
+//    @Override
+//    public void init() {
+//        DcMotorEx liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
+//    }
+//
+//    @Override
+//    public void loop() {
+//        LiftTest();
+//    }
+
