434fc8972eea2b8b54afc501b4499ecc6360b76d
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2024-02-05 18:08:12 -0600

    deal with prop detection on red side

--- TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutoFrontMode.java
+++ TeamCode/src/main/java/edu/edina/opmodes/autonomous/AutoFrontMode.java
@@ -2,9 +2,9 @@ package edu.edina.opmodes.autonomous;
 
 import static edu.edina.library.util.drivecontrol.DriveDirection.Axial;
 import static edu.edina.library.util.drivecontrol.DriveDirection.Lateral;
-import static edu.edina.opmodes.teleop.test.PropDetectingVisionProcessor.Selected.LEFT;
-import static edu.edina.opmodes.teleop.test.PropDetectingVisionProcessor.Selected.MIDDLE;
-import static edu.edina.opmodes.teleop.test.PropDetectingVisionProcessor.Selected.RIGHT;
+import static edu.edina.opmodes.autonomous.AutoMode.SelectedSpike.BACKSTAGE;
+import static edu.edina.opmodes.autonomous.AutoMode.SelectedSpike.MIDDLE;
+import static edu.edina.opmodes.autonomous.AutoMode.SelectedSpike.AUDIENCE;
 
 import edu.edina.library.util.GrabberSide;
 import edu.edina.library.util.Position;
@@ -16,42 +16,46 @@ public abstract class AutoFrontMode extends AutoMode {
 
     @Override
     protected void runMainPath() {
-        if (position == LEFT) {
+        if (position == BACKSTAGE) {
             driveToClosestPoint(24, 36, Axial);
-            driveToClosestPoint(24, 46, Lateral);
+            rotateToHeading(-45);
+            driveToClosestPoint(32,39.5, Axial);
             piBot.drop(GrabberSide.Left);
-            driveToClosestPoint(16, 46, Axial);
+            driveToClosestPoint(30, 24, Axial);
         } else if (position == MIDDLE) {
             driveToClosestPoint(33, 36, Axial);
             piBot.drop(GrabberSide.Right);
             driveToClosestPoint(16, 36, Axial);
-        } else if (position == RIGHT) {
-            driveToClosestPoint(24, 36, Axial);
-            driveToClosestPoint(24, 30, Lateral);
-            piBot.drop(GrabberSide.Right);
-            driveToClosestPoint(16, 30, Axial);
+        } else if (position == AUDIENCE) {
+            driveToClosestPoint(18, 36, Axial);
+            rotateToHeading(-135);
+            driveToClosestPoint(33,33.5, Axial);
+            piBot.drop(GrabberSide.Left);
+            driveToClosestPoint(18, 36, Axial);
+//            driveToClosestPoint();
         } else {
             driveToClosestPoint(24, 36, Axial);
             driveToClosestPoint(24, 46, Lateral);
             piBot.drop(GrabberSide.Right);
             driveToClosestPoint(16, 46, Axial);
         }
-//
-//        rotateToPoint(24, 18);
-//        driveToClosestPoint(24, 18, Axial);
-//        rotateToHeading(180);
-//        driveToClosestPoint(36, 18, Lateral);
-//        driveToClosestPoint(36, 13, Axial);
-//        piBot.grab(GrabberSide.Right);
-//        rotateToPoint(58, 21);
-//        driveToClosestPoint(58, 21, Axial);
-//        rotateToPoint(66, 50);
-//        driveToClosestPoint(66, 50, Axial);
-//        rotateToHeading(0);
-//        driveToClosestPoint(66, 110, Axial);
-//        rotateToHeading(180);
-//        driveToClosestPoint(42, 110, Lateral);
-//        //drop pixel
-//        driveToClosestPoint(10, 110, Lateral);
+
+        rotateToPoint(24, 18);
+        driveToClosestPoint(24, 18, Axial);
+        rotateToHeading(180);
+        driveToClosestPoint(36, 18, Lateral);
+        driveToClosestPoint(36, 13, Axial);
+        piBot.grab(GrabberSide.Right);
+        rotateToPoint(58, 21);
+        driveToClosestPoint(58, 21, Axial);
+        rotateToPoint(66, 50);
+        driveToClosestPoint(66, 50, Axial);
+        rotateToHeading(0);
+        driveToClosestPoint(66, 110, Axial);
+        rotateToHeading(180);
+        driveToClosestPoint(42, 110, Lateral);
+
+        //drop pixel
+        driveToClosestPoint(10, 110, Lateral);
     }
 }
