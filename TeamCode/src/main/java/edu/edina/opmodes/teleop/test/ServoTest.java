44ec3bec2e19435c4b952a48084d6c4602ed52b4
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2024-02-10 09:07:19 -0600

    test servo full power mode

--- TeamCode/src/main/java/edu/edina/opmodes/teleop/test/ServoTest.java
+++ TeamCode/src/main/java/edu/edina/opmodes/teleop/test/ServoTest.java
@@ -3,6 +3,7 @@ package edu.edina.opmodes.teleop.test;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.PwmControl;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import edu.edina.library.util.RobotHardware;
@@ -22,11 +23,23 @@ public class ServoTest extends LinearOpMode {
                 telemetry.addData("lift", "raise");
                 telemetry.update();
 
+                ((PwmControl)hw.intakeSwingLeft).setPwmDisable();
+                ((PwmControl)hw.intakeSwingRight).setPwmDisable();
+                ((PwmControl)hw.intakeLeft).setPwmDisable();
+                ((PwmControl)hw.hangLeft).setPwmDisable();
+                ((PwmControl)hw.hangRight).setPwmDisable();
+                // ((PwmControl)hw.droneLauncher).setPwmDisable();
+
+                hw.hangLiftLeft.setPosition(0.1);
+                hw.hangLiftRight.setPosition(0.9);
+
+
+                sleep(1000);
+
+                ((PwmControl)hw.hangLeft).setPwmEnable();
+                ((PwmControl)hw.hangRight).setPwmEnable();
                 hw.hangRight.setPosition(Servo.MAX_POSITION - 0.05);
                 hw.hangLeft.setPosition(Servo.MIN_POSITION + 0.05);
-                hw.hangLiftLeft.setPosition(0.1);
-//                sleep(1000);
-                hw.hangLiftRight.setPosition(0.9);
             }
             if (gamepad1.left_bumper) {
                 hw.hangRight.setPosition(Servo.MIN_POSITION + 0.05);
