575539a7bc19d74bad5a4dcb91fe8be0231dc2b9
Avner Hegerle <avner.hegerle@gmail.com>
2024-07-19 22:10:19 -0500

    "working"

--- TeamCode/src/main/java/edu/edina/opmodes/teleop/test/OdometryTest.java
+++ TeamCode/src/main/java/edu/edina/opmodes/teleop/test/OdometryTest.java
@@ -5,17 +5,21 @@ import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.FLOAT;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import edu.edina.library.util.RobotHardware;
 
 @TeleOp
 public class OdometryTest extends LinearOpMode {
-    private RobotHardware hw;
+
 
     @Override
     public void runOpMode() {
-        hw = new RobotHardware(hardwareMap);
-        DcMotor[] motors = new DcMotor[]{hw.frontLeftMotor, hw.backLeftMotor, hw.frontRightMotor, hw.backRightMotor};
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
+        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
+        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        DcMotor[] motors = new DcMotor[]{frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor};
         String[] positions = new String[]{"FL", "BL", "FR", "BR"};
         for (int i = 0; i < 4; i++) {
             motors[i].setZeroPowerBehavior(FLOAT);
@@ -26,7 +30,7 @@ public class OdometryTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             for (int i = 0; i < 4; i++) {
-                telemetry.addData(positions[i], "%.1d", motors[i].getCurrentPosition());
+                telemetry.addData(positions[i], motors[i].getCurrentPosition());
             }
 
             telemetry.update();
