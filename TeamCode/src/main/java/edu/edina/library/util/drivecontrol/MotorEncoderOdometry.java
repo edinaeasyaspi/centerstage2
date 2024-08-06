06676321c98fc18d7c92459cd35a8754abc36ff3
Avner Hegerle <avner.hegerle@gmail.com>
2024-08-05 21:13:13 -0500

    Add motor encoder class

---
+++ TeamCode/src/main/java/edu/edina/library/util/drivecontrol/MotorEncoderOdometry.java
@@ -0,0 +1,24 @@
+package edu.edina.library.util.drivecontrol;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class MotorEncoderOdometry {
+    private static final double POS_TO_INCH = (1114.0 / 360.0) / 37.5;
+    private final DcMotorEx motor;
+    private final double mult;
+    private final double offset;
+    private final int startPos;
+
+    public MotorEncoderOdometry(DcMotorEx motor, double mult, double offset) {
+        this.motor = motor;
+        this.mult = mult;
+        this.offset = offset;
+        startPos = motor.getCurrentPosition();
+    }
+
+    public double estRadius(double yawRadians) {
+        double pos = startPos - motor.getCurrentPosition() * mult;
+        double distance = pos / POS_TO_INCH;
+        return distance / yawRadians + offset;
+    }
+}
--- TeamCode/src/main/java/edu/edina/opmodes/teleop/test/OdometryTest.java
+++ TeamCode/src/main/java/edu/edina/opmodes/teleop/test/OdometryTest.java
@@ -5,6 +5,7 @@ import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENC
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.FLOAT;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADIANS;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -17,18 +18,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
 public class OdometryTest extends LinearOpMode {
-    private double inches;
-
     @Override
     public void runOpMode() {
         double degMult = 1114.0 / 360.0;
+        double robotWidth = 14.5;
         DcMotor[] motors = new DcMotor[]{
                 hardwareMap.get(DcMotorEx.class, "frontLeftMotor"),
                 hardwareMap.get(DcMotorEx.class, "backLeftMotor"),
-                hardwareMap.get(DcMotorEx.class, "backRightMotor"),
-                hardwareMap.get(DcMotorEx.class, "frontRightMotor")
+                hardwareMap.get(DcMotorEx.class, "frontRightMotor"),
+                hardwareMap.get(DcMotorEx.class, "backRightMotor")
         };
         String[] positions = new String[]{"FL", "BL", "FR", "BR"};
+        double[] motorInches = new double[]{0, 0, 0, 0};
         int[] mult = new int[]{1, 1, -1, -1};
         for (int i = 0; i < 4; i++) {
             motors[i].setZeroPowerBehavior(FLOAT);
@@ -47,12 +48,21 @@ public class OdometryTest extends LinearOpMode {
         imu.resetYaw();
 
         while (opModeIsActive()) {
-            for (int i = 0; i < 4; i++) {
-                double inches = motors[i].getCurrentPosition() * mult[i] / degMult / 37.5;
-                telemetry.addData(positions[i], "%.2fin", inches);
-            }
             YawPitchRollAngles robotOrientation = imu.getRobotYawPitchRollAngles();
+
+            for (int i = 0; i < 4; i++) {
+                motorInches[i] = motors[i].getCurrentPosition() * mult[i] / degMult / 37.5;
+                telemetry.addData(positions[i], "%.2fin", motorInches[i]);
+            }
             telemetry.addData("IMU", robotOrientation.getYaw(DEGREES));
+
+            double a = Math.abs(robotOrientation.getYaw(RADIANS));
+            double dInside = motorInches[2];
+            double radius = (dInside / a) + (robotWidth / 2);
+            double dCenter = a * radius;
+
+            telemetry.addData("radius", radius);
+            telemetry.addData("arc length", dCenter);
             telemetry.update();
         }
     }
